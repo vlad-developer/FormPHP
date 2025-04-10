f853cec4565188ff4519d82c1e958189e717a601
Vlademir Soares <vladssoares@hotmail.com>
2025-04-10 18:59:06 -0300

    Remover Globo

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
 <body>
   <div id="corpoform">
    <form method="post" action="processa.php">
-       <h1>Acesso ao sistema😀</h1>
+       <h1>Acesso ao sistema</h1>
        <input type="email" name="email" placeholder="E-mail">
        <input type="password" name="password" placeholder="Senha">
        <input type="submit" value="Entrar">
       <p>Clique aqui para se <a href="cadastro.php">Cadastrar.</a></p>
+      
           </form>
     </div>
        
