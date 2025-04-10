611902b32954298271f2947f01f2d7eacb5107b6
Vlademir Soares <vladssoares@hotmail.com>
2025-04-10 18:16:34 -0300

    Commit Inicial

---
+++ cadastro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Form with PHP</title>
+<link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+  <div id="corpoform">
+   <form method="post" action="processa.php">
+       <h1>Cadastrar</h1>
+       <input type="text" name="email" placeholder="Nome compelto">
+       <input type="text" name="text" placeholder="Telefone">
+       <input type="text" name="email" placeholder="E-mail">
+       <input type="password" name="password" placeholder="Senha">
+       <input type="password" name="password" placeholder="Confirmar Senha">
+       
+       <input type="submit" value="Cadastrar">
+      <a href="../LoginForm/index.php">Cancelar</a>
+          </form>
+    </div>
+       
+   
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Form with PHP</title>
+<link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+  <div id="corpoform">
+   <form method="post" action="processa.php">
+       <h1>Acesso ao sistema😀</h1>
+       <input type="email" name="email" placeholder="E-mail">
+       <input type="password" name="password" placeholder="Senha">
+       <input type="submit" value="Entrar">
+      <p>Clique aqui para se <a href="cadastro.php">Cadastrar.</a></p>
+          </form>
+    </div>
+       
+   
+</body>
+</html>
